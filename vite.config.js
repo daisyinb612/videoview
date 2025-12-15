c8a8181642369980d449f4f44259e2b96c67c9fa
daisyinb612 <2452825107@qq.com>
2025-12-15 18:51:09 +0800

    1215

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vite 配置
 export default defineConfig({
   plugins: [react()],
+
+  // 部署到 GitHub Pages 时，必须设置 base 为仓库名
+  // 对应访问地址为：https://daisyinb612.github.io/videoview/
+  base: '/videoview/',
+
   server: {
     port: 3000,
     open: true
